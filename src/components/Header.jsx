1749ea8ee5135a6a1652e533f967212909453867
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-11 20:56:06 +0400

    enhance header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ import CTAButtons from "./CTAButtons";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between">
+    <header className="mb-10 flex items-center justify-between py-7.5">
       <div className="flex items-center gap-2">
-        <span className="text-purple text-[1.9375rem]">Momentum</span>
+        <span className="text-purple font-fredoka-one text-[1.9375rem]">
+          Momentum
+        </span>
         <Logo />
       </div>
 
