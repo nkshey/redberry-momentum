7eedcd1cb345982e454d47b4cee04b5a771e7c48
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-12 21:27:34 +0400

    update primary button styling

--- src/ui/buttons/PrimaryButton.jsx
+++ src/ui/buttons/PrimaryButton.jsx
@@ -1,7 +1,7 @@
 function PrimaryButton({ className, children }) {
   return (
     <button
-      className={`bg-purple hover:bg-light-purple flex cursor-pointer items-center gap-1 rounded-[0.3125rem] px-5 py-2.5 leading-[1em] text-white transition-colors ${className}`}
+      className={`bg-purple hover:bg-light-purple flex cursor-pointer items-center gap-1 rounded-[0.3125rem] px-5 py-2.5 text-white transition-colors ${className}`}
     >
       {children}
     </button>
