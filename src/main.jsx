3df76d5378a819d3216b1aafd1c0cb2775e0fcd9
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-13 00:04:23 +0400

    add route for AddTaskPage component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./index.css";
 import App from "./App.jsx";
 import HomePage from "./pages/HomePage.jsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import TaskPage from "./pages/TaskPage.jsx";
+import AddTaskPage from "./pages/AddTaskPage.jsx";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <HomePage />,
+      },
+      {
+        path: "/add",
+        element: <AddTaskPage />,
       },
       {
         path: "/task/:id",
