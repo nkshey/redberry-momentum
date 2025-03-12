090484c99c48e797a672da4905d50b2f6a29aca5
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-12 21:34:14 +0400

    add API fetchers

---
+++ src/api/fetchers.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+
+const token = import.meta.env.VITE_REDBERRY_TOKEN;
+
+export async function fetchStatuses() {
+  const { data } = await axios.get(
+    "https://momentum.redberryinternship.ge/api/statuses",
+  );
+  return data;
+}
+
+export async function fetchTasks() {
+  const { data } = await axios.get(
+    "https://momentum.redberryinternship.ge/api/tasks",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+  return data;
+}
+
+export async function getTaskById(id) {
+  const { data } = await axios.get(
+    `https://momentum.redberryinternship.ge/api/tasks/${id}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+  return data;
+}
+
+export async function fetchDepartments() {
+  const { data } = await axios.get(
+    "https://momentum.redberryinternship.ge/api/departments",
+  );
+  return data;
+}
+
+export async function fetchPriorities() {
+  const { data } = await axios.get(
+    "https://momentum.redberryinternship.ge/api/priorities",
+  );
+  return data;
+}
+
+export async function fetchEmployees() {
+  const { data } = await axios.get(
+    "https://momentum.redberryinternship.ge/api/employees",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+  return data;
+}
