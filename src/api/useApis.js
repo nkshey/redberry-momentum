d89a9322411ce65457ec224375f13f2583def6ac
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-12 21:34:20 +0400

    add custom hooks for API data fetching

---
+++ src/api/useApis.js
@@ -0,0 +1,51 @@
+import { useQuery } from "@tanstack/react-query";
+import {
+  fetchStatuses,
+  fetchTasks,
+  getTaskById,
+  fetchDepartments,
+  fetchPriorities,
+  fetchEmployees,
+} from "./fetchers";
+
+export function useStatuses() {
+  return useQuery({
+    queryKey: ["statuses"],
+    queryFn: fetchStatuses,
+  });
+}
+
+export function useTasks() {
+  return useQuery({
+    queryKey: ["tasks"],
+    queryFn: fetchTasks,
+  });
+}
+
+export function useSpecificTask(id) {
+  return useQuery({
+    queryKey: ["task", id],
+    queryFn: () => getTaskById(id),
+  });
+}
+
+export function useDepartments() {
+  return useQuery({
+    queryKey: ["departments"],
+    queryFn: fetchDepartments,
+  });
+}
+
+export function usePriorities() {
+  return useQuery({
+    queryKey: ["priorities"],
+    queryFn: fetchPriorities,
+  });
+}
+
+export function useEmployees() {
+  return useQuery({
+    queryKey: ["employees"],
+    queryFn: fetchEmployees,
+  });
+}
