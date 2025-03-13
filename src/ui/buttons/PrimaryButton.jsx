cb6ec173bbbdff05a312082f7ac767ccce317685
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-13 14:57:23 +0400

    add onClick handler

--- src/ui/buttons/PrimaryButton.jsx
+++ src/ui/buttons/PrimaryButton.jsx
@@ -1,7 +1,8 @@
-function PrimaryButton({ className, children }) {
+function PrimaryButton({ className, children, onClick }) {
   return (
     <button
       className={`bg-purple hover:bg-light-purple flex cursor-pointer items-center gap-1 rounded-[0.3125rem] px-5 py-2.5 text-white transition-colors ${className}`}
+      onClick={onClick}
     >
       {children}
     </button>
--- src/ui/buttons/SecondaryButton.jsx
+++ src/ui/buttons/SecondaryButton.jsx
@@ -1,7 +1,8 @@
-function SecondaryButton({ className, children }) {
+function SecondaryButton({ className, children, onClick }) {
   return (
     <button
       className={`border-purple hover:border-light-purple flex cursor-pointer items-center gap-1 rounded-[0.3125rem] border bg-white px-5 py-2.5 leading-[1em] transition-colors ${className}`}
+      onClick={onClick}
     >
       {children}
     </button>
