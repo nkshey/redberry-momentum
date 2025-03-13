321961718ccbc958ea78d7da81ad8845fb3fc1d5
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-13 14:57:00 +0400

    refactor CheckBox component to utilize filter state and improve rendering logic

--- src/ui/inputs/CheckBox.jsx
+++ src/ui/inputs/CheckBox.jsx
@@ -1,22 +1,53 @@
+import useFilterStore, { FILTER_TYPES } from "../../stores/useFilterStore";
 import CheckMarkIcon from "../icons/CheckMarkIcon";
 
-function CheckBox({ id, children }) {
+function CheckBox({ filter, filterType, onChange }) {
+  const temp = useFilterStore((state) => state.temp);
+
+  // Check only the specific filter type
+  let isChecked = false;
+
+  if (filterType === FILTER_TYPES.DEPARTMENTS) {
+    isChecked = temp.departments.some((item) => item.id === filter.id);
+  } else if (filterType === FILTER_TYPES.PRIORITIES) {
+    isChecked = temp.priorities.some((item) => item.id === filter.id);
+  } else if (filterType === FILTER_TYPES.EMPLOYEE) {
+    isChecked = temp.employee !== null && temp.employee.id === filter.id;
+  }
+
+  const isEmployee = filterType === FILTER_TYPES.EMPLOYEE;
+
   return (
     <label
       className="flex w-fit cursor-pointer items-center gap-3.5"
-      htmlFor={id}
+      htmlFor={filter.id}
     >
       <div className="relative grid">
         <input
           className="peer checked:border-purple relative size-5.5 cursor-pointer appearance-none rounded-md border-[2px] bg-white"
           type="checkbox"
-          id={id}
+          id={filter.id}
+          checked={isChecked}
+          onChange={onChange}
         />
 
         <CheckMarkIcon className="peer-checked:stroke-purple pointer-events-none absolute top-1/2 left-1/2 hidden -translate-x-1/2 -translate-y-1/2 stroke-white peer-checked:block" />
       </div>
 
-      <div className="text-sm leading-[1em]">{children}</div>
+      {isEmployee ? (
+        <div className="flex items-center gap-[0.625rem]">
+          <img
+            className="size-7 rounded-full object-cover"
+            src={filter.avatar}
+            alt={`${filter.name} ${filter.surname}`}
+          />
+          <span>
+            {filter.name} {filter.surname}
+          </span>
+        </div>
+      ) : (
+        <span className="text-sm leading-[1em]">{filter.name}</span>
+      )}
     </label>
   );
 }
