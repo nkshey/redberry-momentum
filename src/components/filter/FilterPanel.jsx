86ebbfebe8a463d75aabc34555c429482eadb125
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-13 14:58:01 +0400

    add onClick handler to PrimaryButton in FilterPanel for apply action

--- src/components/filter/FilterPanel.jsx
+++ src/components/filter/FilterPanel.jsx
@@ -1,14 +1,17 @@
 import PrimaryButton from "../../ui/buttons/PrimaryButton";
 
-function FilterPanel({ ref, children }) {
+function FilterPanel({ ref, children, onApply }) {
   return (
     <div
-      className="border-purple absolute top-[calc(100%+0.625rem)] -left-[1px] w-fit min-w-[calc(100%+2px)] rounded-[0.625rem] border bg-white px-7.5 pt-10 pb-5"
+      className="border-purple absolute top-[calc(100%+0.625rem)] -left-[1px] z-99 w-fit min-w-[calc(100%+2px)] rounded-[0.625rem] border bg-white px-7.5 pt-10 pb-5"
       ref={ref}
     >
       {children}
 
-      <PrimaryButton className="mt-6 h-9 place-self-end rounded-full px-12.5">
+      <PrimaryButton
+        className="mt-6 h-9 place-self-end rounded-full px-12.5"
+        onClick={onApply}
+      >
         არჩევა
       </PrimaryButton>
     </div>
