cea92af1c2c2c684f2b19f0959bc5789505895c7
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-13 14:58:17 +0400

    add useFilterStore to TasksList for temporary filter state access

--- src/components/task/TasksList.jsx
+++ src/components/task/TasksList.jsx
@@ -1,10 +1,14 @@
 import { useStatuses, useTasks } from "../../api/useApis";
+import useFilterStore from "../../stores/useFilterStore";
 import TaskCard from "./TaskCard";
 import TaskStatus from "./TaskStatus";
 
 function TasksList() {
   const { data: statuses, isLoading: statusesLoading } = useStatuses();
   const { data: tasks, isLoading: tasksLoading } = useTasks();
+  const temp = useFilterStore((state) => state.temp);
+
+  console.log(temp);
 
   if (statusesLoading || tasksLoading) return <div>Loading...</div>;
 
