cbec7ab67d3acebc057fb66211179bcdd947f43d
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-13 17:19:21 +0400

    add cursor style to TaskStatus component

--- src/components/task/TaskStatus.jsx
+++ src/components/task/TaskStatus.jsx
@@ -3,7 +3,7 @@ import { statusColors } from "../../utils/constants";
 function TaskStatus({ name }) {
   return (
     <h2
-      className="mb-7.5 grid h-13.5 place-content-center rounded-[0.625rem] px-4 text-center text-[1.25rem] font-medium text-white"
+      className="mb-7.5 grid h-13.5 cursor-default place-content-center rounded-[0.625rem] px-4 text-center text-[1.25rem] font-medium text-white"
       style={{ backgroundColor: statusColors[name] }}
     >
       {name}
