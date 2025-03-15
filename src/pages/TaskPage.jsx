2f43e06e8ffd23e84369646ff8a9f8b99e47053c
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-15 17:25:47 +0400

    refactor TaskPage to improve layout and integrate TaskPageTaskDetails and CommentsSection components

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,50 +1,36 @@
 import { useParams } from "react-router-dom";
-import { useSpecificTask, useStatuses } from "../api/useApis";
-import TaskPageHeader from "../components/task-page/TaskPageHeader";
-import PieChartIcon from "../ui/icons/PieChartIcon";
-import UserIcon from "../ui/icons/UserIcon";
-import CalendarIcon from "../ui/icons/CalendarIcon";
-import ChevronIcon from "../ui/icons/ChevronIcon";
+import { useSpecificTask } from "../api/useApis";
+import TaskPageHeader from "../components/pages/taskpage/TaskPageHeader";
+import TaskPageTaskDetails from "../components/pages/taskpage/TaskPageTaskDetails";
+import CommentsSection from "../components/pages/taskpage/CommentsSection";
 
 function TaskPage() {
   const { id } = useParams();
   const { data: task, isLoading: taskLoading } = useSpecificTask(id);
-  const { data: statuses, isLoading: statusesLoading } = useStatuses();
 
-  if (taskLoading || statusesLoading) return <div>Loading...</div>;
+  if (taskLoading) return <div>Loading...</div>;
 
   return (
-    <section>
-      <TaskPageHeader task={task} />
+    <div className="grid grid-cols-[1fr_1.035fr] gap-56">
+      <section>
+        <TaskPageHeader
+          priority={task.priority}
+          name={task.name}
+          description={task.description}
+        />
 
-      <div>
-        <p className="mb-7 text-2xl font-medium">დავალების დეტალები</p>
+        <TaskPageTaskDetails
+          taskId={task.id}
+          status={task.status}
+          employeeAvatar={task.employee.avatar}
+          employeeDepartment={task.employee.department.name}
+          employeeName={`${task.employee.name} ${task.employee.surname}`}
+          dueDate={task.due_date}
+        />
+      </section>
 
-        <div>
-          <div className="flex items-center gap-17.5">
-            <div className="flex items-center gap-1.5">
-              <PieChartIcon />
-              <span>სტატუსი</span>
-            </div>
-
-            <div className="flex h-11 w-65 items-center justify-between rounded-[5px] border border-[#CED4DA] px-3.5">
-              <span className="font-light">მზად ტესტირებისთვის</span>
-              <ChevronIcon />
-            </div>
-          </div>
-
-          <div className="flex items-center gap-1.5">
-            <UserIcon />
-            <span>თანამშრომელი</span>
-          </div>
-
-          <div className="flex items-center gap-1.5">
-            <CalendarIcon />
-            <span>დავალების ვადა</span>
-          </div>
-        </div>
-      </div>
-    </section>
+      <CommentsSection taskId={id} />
+    </div>
   );
 }
 
