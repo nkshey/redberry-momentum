df55ce7a8a0a65401983c35b7b7a2c44a27d28ab
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-15 17:49:17 +0400

    improve disabled state styling

--- src/ui/buttons/PrimaryButton.jsx
+++ src/ui/buttons/PrimaryButton.jsx
@@ -1,7 +1,7 @@
 function PrimaryButton({ className, children, disabled, onClick }) {
   return (
     <button
-      className={`hover:bg-light-purple flex items-center gap-1 rounded-[0.3125rem] px-5 text-white transition-colors ${disabled ? "bg-light-purple cursor-default" : "bg-purple cursor-pointer"} ${className}`}
+      className={`hover:bg-light-purple flex items-center gap-1 rounded-[0.3125rem] px-5 text-white transition-colors ${disabled ? "bg-light-purple cursor-not-allowed" : "bg-purple cursor-pointer"} ${className}`}
       disabled={disabled}
       onClick={onClick}
     >
