65675d66901831085924857a4bf67eb6c0fb60ce
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-15 17:26:20 +0400

    refactor buttons to support disabled state and improve styling

--- src/ui/buttons/PrimaryButton.jsx
+++ src/ui/buttons/PrimaryButton.jsx
@@ -1,7 +1,8 @@
-function PrimaryButton({ className, children, onClick }) {
+function PrimaryButton({ className, children, disabled, onClick }) {
   return (
     <button
-      className={`bg-purple hover:bg-light-purple flex cursor-pointer items-center gap-1 rounded-[0.3125rem] px-5 py-2.5 text-white transition-colors ${className}`}
+      className={`hover:bg-light-purple flex items-center gap-1 rounded-[0.3125rem] px-5 text-white transition-colors ${disabled ? "bg-light-purple cursor-default" : "bg-purple cursor-pointer"} ${className}`}
+      disabled={disabled}
       onClick={onClick}
     >
       {children}
--- src/ui/buttons/SecondaryButton.jsx
+++ src/ui/buttons/SecondaryButton.jsx
@@ -1,7 +1,7 @@
 function SecondaryButton({ className, children, onClick }) {
   return (
     <button
-      className={`border-purple hover:border-light-purple flex cursor-pointer items-center gap-1 rounded-[0.3125rem] border bg-white px-5 py-2.5 leading-[1em] transition-colors ${className}`}
+      className={`border-purple hover:border-light-purple flex h-10 cursor-pointer items-center gap-1 rounded-[0.3125rem] border bg-white px-5 leading-[1em] transition-colors ${className}`}
       onClick={onClick}
     >
       {children}
