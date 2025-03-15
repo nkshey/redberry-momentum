b421bf9f0d3b9bfad14b9e60503cf6d38a47057b
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-15 17:27:43 +0400

    refactor Dropdown component to enhance functionality and styling

--- src/ui/inputs/Dropdown.jsx
+++ src/ui/inputs/Dropdown.jsx
@@ -1,14 +1,45 @@
+import { useState } from "react";
 import ChevronIcon from "../../ui/icons/ChevronIcon";
 
-function Dropdown({ label }) {
+function Dropdown({
+  className,
+  label,
+  data,
+  isLoading,
+  selected,
+  handleSelect,
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
       <label htmlFor="">{label}</label>
 
-      <div className="border-very-light-gray flex h-11.5 cursor-pointer items-center justify-between rounded-[0.3125rem] border bg-white px-3.5">
-        <span className="text-sm font-light">დასაწყები</span>
-        <ChevronIcon width={14} height={14} />
-      </div>
+      <button
+        className={`relative flex h-11.5 cursor-pointer items-center justify-between rounded-[0.3125rem] border bg-white px-3.5 ${className} ${isOpen ? "border-purple rounded-b-none" : "border-very-light-gray"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span className="text-sm font-light">{selected}</span>
+        <ChevronIcon
+          className={isOpen ? "-rotate-180" : ""}
+          width={14}
+          height={14}
+        />
+
+        {data && isOpen && (
+          <ul className="border-purple absolute top-full -left-[1px] z-99 w-[calc(100%+2px)] rounded-b-[0.3125rem] border border-t-0 bg-white text-sm font-light">
+            {data.map((item, index) => (
+              <li
+                className="hover:text-light-purple active:text-purple flex h-11.5 items-center px-3.5 transition-colors"
+                key={item.id ?? index}
+                onClick={() => handleSelect(item)}
+              >
+                {item.name}
+              </li>
+            ))}
+          </ul>
+        )}
+      </button>
     </div>
   );
 }
