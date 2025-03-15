9c0f4c891a4eeb5e94cc4d3a25991fb0d56fb1f1
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-15 17:26:24 +0400

    refactor SecondaryButton to support disabled state

--- src/ui/buttons/SecondaryButton.jsx
+++ src/ui/buttons/SecondaryButton.jsx
@@ -1,7 +1,8 @@
-function SecondaryButton({ className, children, onClick }) {
+function SecondaryButton({ className, children, disabled, onClick }) {
   return (
     <button
       className={`border-purple hover:border-light-purple flex h-10 cursor-pointer items-center gap-1 rounded-[0.3125rem] border bg-white px-5 leading-[1em] transition-colors ${className}`}
+      disabled={disabled}
       onClick={onClick}
     >
       {children}
