51e0de7ce2bf77df24dfa3e1afa3c5522c27a055
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-15 17:25:07 +0400

    adjust line height of PageTitle in TaskPageHeader

--- src/components/pages/taskpage/TaskPageHeader.jsx
+++ src/components/pages/taskpage/TaskPageHeader.jsx
@@ -5,7 +5,7 @@ function TaskPageHeader({ priority, name, description }) {
   return (
     <div className="mb-15.5">
       <PriorityBadge className="text-base" priority={priority} />
-      <PageTitle className="mt-3 mb-9">{name}</PageTitle>
+      <PageTitle className="mt-3 mb-9 leading-[1em]">{name}</PageTitle>
       <p>{description}</p>
     </div>
   );
