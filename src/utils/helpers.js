6d68ac97d54a5ef8a890612bf2858abf3c46ef1b
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-17 15:41:35 +0400

    add saveToSessionStorage and getFromSessionStorage functions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -27,3 +27,21 @@ export function getTomorrowDate() {
   tomorrow.setDate(tomorrow.getDate() + 1);
   return tomorrow.toISOString().split("T")[0];
 }
+
+export function saveToSessionStorage(key, data) {
+  try {
+    sessionStorage.setItem(key, JSON.stringify(data));
+  } catch (error) {
+    console.error("Error saving to sessionStorage:", error);
+  }
+}
+
+export function getFromSessionStorage(key, defaultValue) {
+  try {
+    const storedValue = sessionStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : defaultValue;
+  } catch (error) {
+    console.error("Error loading from sessionStorage:", error);
+    return defaultValue;
+  }
+}
