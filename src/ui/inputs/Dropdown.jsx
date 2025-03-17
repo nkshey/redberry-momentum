bd15c12401b3534a153ab7762253bbc9efb775cd
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-17 15:19:50 +0400

    refactor Dropdown component to manage selected item state and handle clicks outside the dropdown

--- src/ui/inputs/Dropdown.jsx
+++ src/ui/inputs/Dropdown.jsx
@@ -1,40 +1,69 @@
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import ChevronIcon from "../../ui/icons/ChevronIcon";
+import { useClickOutside } from "../../hooks/useClickOutside";
 
-function Dropdown({
-  className,
-  label,
-  data,
-  isLoading,
-  selected,
-  handleSelect,
-}) {
+const Dropdown = ({ className, label, data, value, onChange, ...props }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const dropdownRef = useRef(null);
+
+  useClickOutside(dropdownRef, () => {
+    if (isOpen) setIsOpen(false);
+  });
+
+  useEffect(() => {
+    setSelectedItem(data?.find((item) => item.id === value));
+  }, [data, value]);
+
+  function handleItemClick(item) {
+    setSelectedItem(item);
+    onChange(item.id);
+    setIsOpen(false);
+  }
 
   return (
-    <div>
-      <label htmlFor="">{label}</label>
+    <div className="dropdown-container flex flex-col" ref={dropdownRef}>
+      <label className="text-gray mb-1.5 leading-[1em]">{label}</label>
 
+      {/* Store the value */}
+      <input type="hidden" value={selectedItem?.id || ""} readOnly {...props} />
+
+      {/* Dropdown button */}
       <button
+        type="button"
         className={`relative flex h-11.5 cursor-pointer items-center justify-between rounded-[0.3125rem] border bg-white px-3.5 ${className} ${isOpen ? "border-purple rounded-b-none" : "border-very-light-gray"}`}
         onClick={() => setIsOpen(!isOpen)}
       >
-        <span className="text-sm font-light">{selected}</span>
+        {/* Show selected item or placeholder */}
+        <div className="text-sm font-light">
+          {selectedItem ? (
+            <div className="flex items-center gap-1.5">
+              {selectedItem.icon && <img src={selectedItem.icon} alt="" />}
+              <span>{selectedItem.name}</span>
+            </div>
+          ) : (
+            "აირჩიე"
+          )}
+        </div>
+
+        {/* Dropdown arrow */}
         <ChevronIcon
           className={isOpen ? "-rotate-180" : ""}
           width={14}
           height={14}
         />
 
-        {data && isOpen && (
+        {/* Dropdown items list */}
+        {isOpen && data && (
           <ul className="border-purple absolute top-full -left-[1px] z-99 w-[calc(100%+2px)] rounded-b-[0.3125rem] border border-t-0 bg-white text-sm font-light">
-            {data.map((item, index) => (
+            {data.map((item) => (
               <li
-                className="hover:text-light-purple active:text-purple flex h-11.5 items-center px-3.5 transition-colors"
-                key={item.id ?? index}
-                onClick={() => handleSelect(item)}
+                key={item.id}
+                className="hover:text-light-purple active:text-purple flex h-11.5 items-center gap-1.5 px-3.5 transition-colors"
+                onClick={() => handleItemClick(item)}
               >
-                {item.name}
+                {item.icon && <img src={item.icon} alt="" />}
+                <span>{item.name}</span>
               </li>
             ))}
           </ul>
@@ -42,6 +71,6 @@ function Dropdown({
       </button>
     </div>
   );
-}
+};
 
 export default Dropdown;
