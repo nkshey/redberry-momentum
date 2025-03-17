e5a70f6bc0b627851778850ae4e44dac3cde58c1
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-17 23:11:28 +0400

    adjust styling to SecondaryButton

--- src/ui/buttons/SecondaryButton.jsx
+++ src/ui/buttons/SecondaryButton.jsx
@@ -1,7 +1,7 @@
 function SecondaryButton({ className, children, disabled, onClick }) {
   return (
     <button
-      className={`border-purple rounded-mdborder flex h-10 items-center gap-1 bg-white px-5 leading-[1em] transition-colors ${disabled ? "cursor-not-allowed opacity-50" : "hover:border-light-purple cursor-pointer"} ${className}`}
+      className={`border-purple flex items-center gap-1 rounded-md border bg-white px-5 leading-[1em] transition-colors ${disabled ? "cursor-not-allowed opacity-50" : "hover:border-light-purple cursor-pointer"} ${className}`}
       disabled={disabled}
       onClick={onClick}
     >
