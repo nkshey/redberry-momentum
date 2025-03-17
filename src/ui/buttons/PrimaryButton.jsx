79edb543013c7a331d8e12654c9a6d91a656b14e
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-17 23:27:45 +0400

    add type prop to Button components

--- src/ui/buttons/PrimaryButton.jsx
+++ src/ui/buttons/PrimaryButton.jsx
@@ -1,7 +1,14 @@
-function PrimaryButton({ className, children, disabled, onClick }) {
+function PrimaryButton({
+  className,
+  type = "button",
+  children,
+  disabled,
+  onClick,
+}) {
   return (
     <button
       className={`hover:bg-light-purple flex items-center gap-1 rounded-md px-5 text-white transition-colors ${disabled ? "bg-light-purple cursor-not-allowed" : "bg-purple cursor-pointer"} ${className}`}
+      type={type}
       disabled={disabled}
       onClick={onClick}
     >
--- src/ui/buttons/SecondaryButton.jsx
+++ src/ui/buttons/SecondaryButton.jsx
@@ -1,7 +1,14 @@
-function SecondaryButton({ className, children, disabled, onClick }) {
+function SecondaryButton({
+  className,
+  type = "button",
+  children,
+  disabled,
+  onClick,
+}) {
   return (
     <button
       className={`border-purple flex items-center gap-1 rounded-md border bg-white px-5 leading-[1em] transition-colors ${disabled ? "cursor-not-allowed opacity-50" : "hover:border-light-purple cursor-pointer"} ${className}`}
+      type={type}
       disabled={disabled}
       onClick={onClick}
     >
