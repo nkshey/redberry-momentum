847ea553165e6e77bf01a4216e56e4f76b3c0edb
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-19 18:54:59 +0400

    add DropdownSkeleton component for loading state

---
+++ src/ui/feedback/DropdownSkeleton.jsx
@@ -0,0 +1,10 @@
+function DropdownSkeleton() {
+  return (
+    <div className="w-full">
+      <div className="bg-very-light-gray mb-1.5 h-4 w-32 animate-pulse rounded-sm"></div>
+      <div className="bg-very-light-gray h-11.5 w-full animate-pulse rounded-md"></div>
+    </div>
+  );
+}
+
+export default DropdownSkeleton;
