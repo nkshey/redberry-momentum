913209ed6e03c88126ac2474f720edb150374030
Nika Peikrishvili <peiqrishvili777@gmail.com>
2025-03-20 20:05:37 +0400

    update FilterTag component to use w-max for button width

--- src/components/filter/FilterTag.jsx
+++ src/components/filter/FilterTag.jsx
@@ -4,7 +4,7 @@ function FilterTag({ id, name, onClick }) {
   return (
     <li key={id}>
       <button
-        className="text-gray flex cursor-pointer items-center gap-1 rounded-full border border-[#CED4DA] px-2.5 py-1.5 text-sm leading-[1em] font-light"
+        className="text-gray flex w-max cursor-pointer items-center gap-1 rounded-full border border-[#CED4DA] px-2.5 py-1.5 text-sm leading-[1em] font-light"
         onClick={onClick}
       >
         <span>{name}</span>
